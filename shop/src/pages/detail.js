28b162d5235d93f2f927a2e8d44504ec89496e12
joonda <kloudzero@kakao.com>
2024-11-28 14:55:24 +0900

    [UPDATE] tab button

--- shop/src/pages/detail.js
+++ shop/src/pages/detail.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Nav from 'react-bootstrap/Nav';
 
 function Detail(props) {
 
@@ -24,9 +25,21 @@ function Detail(props) {
     let product = props.shoes.find(function (x) {
         return x.id == id
     });
-    
+
+    let [tabBtn, setTabbtn] = useState(0);
+
+    let [fade2, setFade2] = useState('');
+
+    useEffect(() => {
+        let a = setTimeout(() => { setFade2('end') }, 10)
+        return () => {
+            clearTimeout(a)
+            setFade2('')
+        }
+    },  [])
+
     return (
-        <div className="container">
+        <div className={`container start ${fade2}`}>
             {
                 showAlert === true
                     ? <div id="second" className="alert alert-warning">2초 이내 구매시 할인!</div> : null
@@ -45,8 +58,46 @@ function Detail(props) {
                 </div>
             </div>
 
+            <Nav variant="tabs" defaultActiveKey="link0">
+                <Nav.Item>
+                    <Nav.Link onClick={() => { setTabbtn(0) }} eventKey="link0">Option 1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link onClick={() => { setTabbtn(1) }} eventKey="link1">Option 2</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link onClick={() => { setTabbtn(2) }} eventKey="link2">Option 3</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <TabContent tabBtn={tabBtn} />
         </div>
     )
 }
+
+function TabContent(props) {
+
+    let [fade, setFade] = useState('')
+
+    useEffect(() => {
+        // automatic batching
+        let a = setTimeout(() => { setFade('end') }, 10) 
+        return () => {
+            clearTimeout(a)
+            setFade('')
+        }
+    }, [props.tabBtn])
+
+    if (props.tabBtn == 0) {
+        return <div className={`start ${fade}`}>내용 0</div>
+    }
+    if (props.tabBtn == 1) {
+        return <div className={`start ${fade}`}>내용 1</div>
+    }
+    if (props.tabBtn == 2) {
+        return <div className={`start ${fade}`}>내용 2</div>
+    }
+}
+
+
 
 export default Detail;
